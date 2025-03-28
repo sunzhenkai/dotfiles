17bfcf777c414a47cf3562c2084d82ae5c4a3bc6
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-28 12:43:44 +0800

    update

---
+++ nvim/lua/plugins/go.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    -- branch = "develop"
+    -- (optional) will update plugin's deps on every update
+    build = function()
+      vim.cmd.GoInstallBinaries()
+    end,
+    ---@type gopher.Config
+    opts = {},
+  },
+}
