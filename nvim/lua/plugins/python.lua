cf4e2e3a3fcb4fb57aec06de7121d844a5132f2f
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-06 20:11:00 +0800

    add python

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,48 +1,58 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "ninja", "rst" } },
-  },
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       ruff = {
-  --         cmd_env = { RUFF_TRACE = "messages" },
-  --         init_options = {
-  --           settings = {
-  --             logLevel = "error",
-  --           },
-  --         },
-  --         keys = {
-  --           {
-  --             "<leader>co",
-  --             LazyVim.lsp.action["source.organizeImports"],
-  --             desc = "Organize Imports",
-  --           },
-  --         },
-  --       },
-  --       ruff_lsp = {
-  --         keys = {
-  --           {
-  --             "<leader>co",
-  --             LazyVim.lsp.action["source.organizeImports"],
-  --             desc = "Organize Imports",
-  --           },
-  --         },
-  --       },
-  --     },
-  --     setup = {
-  --       [ruff] = function()
-  --         LazyVim.lsp.on_attach(function(client, _)
-  --           -- Disable hover in favor of Pyright
-  --           client.server_capabilities.hoverProvider = false
-  --         end, ruff)
-  --       end,
-  --     },
-  --   },
-  -- },
-  {
-    "nvim-neotest/neotest-python",
-  },
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			"mfussenegger/nvim-dap-python", --optional
+			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		},
+		lazy = false,
+		branch = "regexp", -- This is the regexp branch, use this for the new version
+		keys = {
+			{ ",v", "<cmd>VenvSelect<cr>" },
+		},
+		---@type venv-selector.Config
+		opts = {
+			-- Your settings go here
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- pyright will be automatically installed with mason and loaded with lspconfig
+				pyright = {
+					-- settings = {
+					-- 	python = {
+					-- 		venvPath = vim.fn.getcwd(),
+					-- 		venv = "venv",
+					-- 	},
+					-- },
+				},
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = { ensure_installed = { "ninja", "rst" } },
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			-- https://github.com/williamboman/mason-lspconfig.nvim
+			ensure_installed = {
+				"pylint",
+				"debugpy",
+				"pydocstyle",
+				-- "python-lsp-server",
+				"pyright",
+			},
+		},
+	},
+	{
+		"nvim-neotest/neotest-python",
+	},
 }
