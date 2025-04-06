079a897b662405360634a1fbf49c2b33fe820572
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-07 02:25:53 +0800

    update

--- nvim/lua/plugins/cpp.lua
+++ nvim/lua/plugins/cpp.lua
@@ -107,15 +107,15 @@ return {
 					capabilities = {
 						offsetEncoding = { "utf-16" },
 					},
-					cmd = {
-						"clangd",
-						"--background-index",
-						"--clang-tidy",
-						"--header-insertion=iwyu",
-						"--completion-style=detailed",
-						"--function-arg-placeholders",
-						"--fallback-style=llvm",
-					},
+					-- cmd = {
+					-- 	"clangd",
+					-- 	"--background-index",
+					-- 	"--clang-tidy",
+					-- 	"--header-insertion=iwyu",
+					-- 	"--completion-style=detailed",
+					-- 	"--function-arg-placeholders",
+					-- 	"--fallback-style=llvm",
+					-- },
 					init_options = {
 						usePlaceholders = true,
 						completeUnimported = true,
