2caebca1024cb252a7b35b6a295e284f30502d8f
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-08 12:43:09 +0800

    fix cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,6 +1,79 @@
 -- cmp: completion
 return {
   {
+    "hrsh7th/nvim-cmp",
+    enabled = true,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/cmp-vsnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+    -- opts = function(_, opts)
+    --   local has_words_before = function()
+    --     unpack = unpack or table.unpack
+    --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --     return col ~= 0
+    --         and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    --   end
+    --
+    --   local cmp = require("cmp")
+    --
+    --   opts.mapping = vim.tbl_extend("force", opts.mapping, {
+    --     ["<Tab>"] = cmp.mapping(function(fallback)
+    --       if cmp.visible() then
+    --         -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+    --         cmp.select_next_item()
+    --       elseif vim.snippet.active({ direction = 1 }) then
+    --         vim.schedule(function()
+    --           vim.snippet.jump(1)
+    --         end)
+    --       elseif has_words_before() then
+    --         cmp.complete()
+    --       else
+    --         fallback()
+    --       end
+    --     end, { "i", "s" }),
+    --     ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --       if cmp.visible() then
+    --         cmp.select_prev_item()
+    --       elseif vim.snippet.active({ direction = -1 }) then
+    --         vim.schedule(function()
+    --           vim.snippet.jump(-1)
+    --         end)
+    --       else
+    --         fallback()
+    --       end
+    --     end, { "i", "s" }),
+    --   })
+    -- end,
+  },
+  {
     "saghen/blink.compat",
     enabled = false,
     -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
@@ -152,51 +225,6 @@ return {
       end
 
       require("blink.cmp").setup(opts)
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- LSP 补全源
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-            and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
     end,
   },
 }
