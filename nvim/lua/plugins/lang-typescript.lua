0ec2a9a84c882085273668a780cd017d28573718
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-11 12:46:09 +0800

    update

--- nvim/lua/plugins/lang-typescript.lua
+++ nvim/lua/plugins/lang-typescript.lua
@@ -175,10 +175,19 @@ return {
 	},
 	{
 		"williamboman/mason.nvim",
-		opts = function(_, opts)
-			opts.ensure_installed = opts.ensure_installed or {}
-			table.insert(opts.ensure_installed, "js-debug-adapter")
-		end,
+		opts = {
+			ensure_installed = {
+				"typescript-language-server",
+			},
+		},
+	},
+	{
+		"williamboman/mason-lsconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"typescript-language-server",
+			},
+		},
 	},
 	{
 		"echasnovski/mini.icons",
