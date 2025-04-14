7f070d2280b8c1f79d343eca43a44aecd9e0794a
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-14 11:41:30 +0800

    enhance go

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -39,6 +39,13 @@ return {
 					sh = { "shfmt" },
 					c = { "clang_format" },
 					cpp = { "clang_format" },
+					go = { "gofmt" },
+					markdown = { "prettier" },
+					yaml = { "prettier" },
+					json = { "prettier" },
+					css = { "prettier" },
+					typescript = { "prettier" },
+					python = { "isort", "black" },
 				},
 				formatters = {
 					shfmt = {
@@ -112,6 +119,7 @@ return {
 				"cpp",
 				"c",
 				"make",
+				"go",
 			},
 			incremental_selection = {
 				enable = true,
@@ -174,6 +182,8 @@ return {
 				cpp = { "cpplint" },
 				-- fish
 				fish = { "fish" },
+				-- go
+				go = { "golangcilint" },
 			},
 			-- LazyVim extension to easily override linter options
 			-- or add custom linters.
@@ -264,6 +274,91 @@ return {
 				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
 				callback = M.debounce(100, M.lint),
 			})
+		end,
+	},
+	-- numToStr/Comment.nvim
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+	},
+	-- folke/trouble.nvim
+	{
+		"folke/trouble.nvim",
+		-- opts will be merged with the parent spec
+		opts = { use_diagnostic_signs = true },
+	},
+	-- danymat/neogen
+	{
+		"danymat/neogen",
+		config = function()
+			require("neogen").setup({ snippet_engine = "luasnip" })
+		end,
+		keys = {
+			{
+				"<Leader>znc",
+				"<cmd>lua require('neogen').generate({ type = 'class' })<CR>",
+				desc = "Generate Class Documentation",
+			},
+			{
+				"<Leader>znd",
+				"<cmd>lua require('neogen').generate({ type = 'file' })<CR>",
+				desc = "Generate File Documentation",
+			},
+			{
+				"<Leader>znf",
+				"<cmd>lua require('neogen').generate({ type = 'func' })<CR>",
+				desc = "Generate Function Documentation",
+			},
+		},
+		-- Uncomment next line if you want to follow only stable versions
+		-- version = "*"
+	},
+	-- echasnovski/mini.pairs
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+		config = function(_, opts)
+			LazyVim.mini.pairs(opts)
+		end,
+	},
+	-- folke/lazydev.nvim
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		cmd = "LazyDev",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "LazyVim", words = { "LazyVim" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+				{ path = "lazy.nvim", words = { "LazyVim" } },
+			},
+		},
+	},
+	-- code folder
+	-- kevinhwang91/nvim-ufo
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			"neovim/nvim-lspconfig",
+		},
+		setup = function()
+			require("ufo").setup()
 		end,
 	},
 }
