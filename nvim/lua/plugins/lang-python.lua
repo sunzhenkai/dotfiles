a15bba7dddfa07d482008f12a53bd025e12c151f
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-16 15:33:09 +0800

    mod

--- nvim/lua/plugins/lang-python.lua
+++ nvim/lua/plugins/lang-python.lua
@@ -87,16 +87,6 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		opts = function(_, opts)
-			local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
-			for _, server in ipairs(servers) do
-				opts.servers[server] = opts.servers[server] or {}
-				opts.servers[server].enabled = server == lsp or server == ruff
-			end
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
 		dependencies = {},
 		opts = {
 			servers = {
