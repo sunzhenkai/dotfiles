ed1d0d9503a7b42234ad94d5aeac206ca03cad7b
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-18 15:00:12 +0800

    disable image

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -1,77 +1,77 @@
 return {
-	{
-		"vhyrro/luarocks.nvim",
-		priority = 1001, -- this plugin needs to run before anything else
-		opts = {
-			rocks = { "magick" },
-		},
-	},
-	{
-		"3rd/image.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"luarocks.nvim",
-		},
-		opts = {
-			backend = "kitty",
-			integrations = {
-				markdown = {
-					enabled = true,
-					clear_in_insert_mode = false,
-					download_remote_images = true,
-					only_render_image_at_cursor = false,
-					filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-				},
-				neorg = {
-					enabled = true,
-					clear_in_insert_mode = false,
-					download_remote_images = true,
-					only_render_image_at_cursor = false,
-					filetypes = { "norg" },
-				},
-			},
-			max_width = nil,
-			max_height = nil,
-			max_width_window_percentage = nil,
-			max_height_window_percentage = 50,
-			kitty_method = "normal",
-		},
-	},
-	{
-		"3rd/diagram.nvim",
-		dependencies = {
-			"3rd/image.nvim",
-		},
-		opts = { -- you can just pass {}, defaults below
-			events = {
-				render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
-				clear_buffer = { "BufLeave" },
-			},
-			renderer_options = {
-				mermaid = {
-					background = nil, -- nil | "transparent" | "white" | "#hex"
-					theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
-					scale = 1, -- nil | 1 (default) | 2  | 3 | ...
-					width = nil, -- nil | 800 | 400 | ...
-					height = nil, -- nil | 600 | 300 | ...
-				},
-				plantuml = {
-					charset = nil,
-				},
-				d2 = {
-					theme_id = nil,
-					dark_theme_id = nil,
-					scale = nil,
-					layout = nil,
-					sketch = nil,
-				},
-				gnuplot = {
-					size = nil, -- nil | "800,600" | ...
-					font = nil, -- nil | "Arial,12" | ...
-					theme = nil, -- nil | "light" | "dark" | custom theme string
-				},
-			},
-		},
-	},
+	-- {
+	-- 	"vhyrro/luarocks.nvim",
+	-- 	priority = 1001, -- this plugin needs to run before anything else
+	-- 	opts = {
+	-- 		rocks = { "magick" },
+	-- 	},
+	-- },
+	-- {
+	-- 	"3rd/image.nvim",
+	-- 	event = "VeryLazy",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"luarocks.nvim",
+	-- 	},
+	-- 	opts = {
+	-- 		backend = "kitty",
+	-- 		integrations = {
+	-- 			markdown = {
+	-- 				enabled = true,
+	-- 				clear_in_insert_mode = false,
+	-- 				download_remote_images = true,
+	-- 				only_render_image_at_cursor = false,
+	-- 				filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+	-- 			},
+	-- 			neorg = {
+	-- 				enabled = true,
+	-- 				clear_in_insert_mode = false,
+	-- 				download_remote_images = true,
+	-- 				only_render_image_at_cursor = false,
+	-- 				filetypes = { "norg" },
+	-- 			},
+	-- 		},
+	-- 		max_width = nil,
+	-- 		max_height = nil,
+	-- 		max_width_window_percentage = nil,
+	-- 		max_height_window_percentage = 50,
+	-- 		kitty_method = "normal",
+	-- 	},
+	-- },
+	-- {
+	-- 	"3rd/diagram.nvim",
+	-- 	dependencies = {
+	-- 		"3rd/image.nvim",
+	-- 	},
+	-- 	opts = { -- you can just pass {}, defaults below
+	-- 		events = {
+	-- 			render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
+	-- 			clear_buffer = { "BufLeave" },
+	-- 		},
+	-- 		renderer_options = {
+	-- 			mermaid = {
+	-- 				background = nil, -- nil | "transparent" | "white" | "#hex"
+	-- 				theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+	-- 				scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+	-- 				width = nil, -- nil | 800 | 400 | ...
+	-- 				height = nil, -- nil | 600 | 300 | ...
+	-- 			},
+	-- 			plantuml = {
+	-- 				charset = nil,
+	-- 			},
+	-- 			d2 = {
+	-- 				theme_id = nil,
+	-- 				dark_theme_id = nil,
+	-- 				scale = nil,
+	-- 				layout = nil,
+	-- 				sketch = nil,
+	-- 			},
+	-- 			gnuplot = {
+	-- 				size = nil, -- nil | "800,600" | ...
+	-- 				font = nil, -- nil | "Arial,12" | ...
+	-- 				theme = nil, -- nil | "light" | "dark" | custom theme string
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
