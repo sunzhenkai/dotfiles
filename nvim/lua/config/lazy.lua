d0d4fa8d187826c85a050316bb7963441913c56c
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-21 17:02:08 +0800

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
+		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "plugins" },
 	},
 	defaults = {
