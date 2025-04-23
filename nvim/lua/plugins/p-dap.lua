bc1bf0191bb6d6379512196d2d96ca207f9fed41
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-23 16:14:25 +0800

    add neotest & update init

--- nvim/lua/plugins/p-dap.lua
+++ nvim/lua/plugins/p-dap.lua
@@ -3,4 +3,23 @@ return {
 		"mfussenegger/nvim-dap",
 		config = function() end,
 	},
+	{
+		"alfaix/neotest-gtest",
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			-- c++, gtest
+			"alfaix/neotest-gtest",
+		},
+		opts = {
+			adapters = {
+				["neotest-gtest"] = {},
+			},
+		},
+	},
 }
